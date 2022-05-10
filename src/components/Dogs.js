4c3525dd261930be189d7772eccd05e5da552add
Alfredo Parra <alfredo-920@hotmail.com>
2022-05-09 21:51:58 -0500

    Successful connection to API

--- src/components/Dogs.js
+++ src/components/Dogs.js
@@ -1,15 +1,19 @@
 import useSWR from 'swr';
 import React from 'react';
 
-function Dogs() {
-    const URL = "https://api.thedogapi.com/v1/images/search";
-    const [data, error] = useSWR(URL, fetch);
+const URL = "https://api.thedogapi.com/v1/images/search";
+const fetcher = (...args) => fetch(...args).then(res => res.json());
 
-    if (error) return <div>Error al cargar</div>
-    if (!data) return <div>Cargando</div>
-    console.log(data)
+function Dogs() {
+    const { data, error } = useSWR(URL, fetcher);
+
+    console.table(data);
+    console.log(error);
+    if (error) return <div>Error al cargar</div>;
+    if (!data) return <div>Cargando</div>;
+
     return (
-        <div>data</div>
+        <div><img src={data[0].url} key={data[0].id} alt="" /></div>
     );
 }
 
